7822c168f96bd71634fb8a1137737085d1facc79
Galen Ferrara <galen.ferrara557@gmail.com>
2025-06-20 12:28:55 +0100

    Enabled user control of strategy through CLI interface

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ from strategies.ma_crossover import run_ma_crossover_strategy
 from strategies.macd_strategy import run_macd_strategy
 
 def main():
-    print("Welcome to Galen's trading strategy testing grounds!")
+    print("Welcome to Galen's trading strategy testing grounds!\n" \
+    "This program will run a trading strategy on a stock of your choice.\n" \
+    "Please enter a stock ticker: ")
+    print("Enter a strategy: \n" \
+    "\tMA Crossover\n" \
+    "\tMACD")
+    strategy =input("Select a trading strategy:")
     data = get_price_data('NVDA', '2024-06-20', '2025-06-20') # Assign stock and testing period.
-    strategy_name = "MACD" # Assign strategy to run.
+    # strategy_name = "MACD" # Assign strategy to run.
 
-    if strategy_name == "MA Crossover":
+    if strategy == "MA Crossover":
         print("Running Moving Average Crossover Strategy...")
         data = run_ma_crossover_strategy(data)
         run_backtest(data)
-    elif strategy_name == "MACD":
+    elif strategy == "MACD":
         print("Running MACD Strategy...")
         data = run_macd_strategy(data) 
         run_backtest(data)
