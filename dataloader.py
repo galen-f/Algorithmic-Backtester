a0366bc4631ac6cbead0e256e3ddc29cf447395c
Galen Ferrara <galen.ferrara557@gmail.com>
2025-06-26 17:30:35 +0100

    Cleaned close data

--- dataloader.py
+++ dataloader.py
@@ -4,4 +4,9 @@ import pandas as pd
 def get_price_data(ticker, start_date, end_date):
     data = yf.download(ticker, start=start_date, end=end_date)
     data = pd.DataFrame(data)
+
+    # Flatten column names if they are MultiIndex
+    if isinstance(data.columns, pd.MultiIndex):
+        data.columns = data.columns.droplevel(1)
+    
     return data
