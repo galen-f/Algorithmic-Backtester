60fe099f89c6a55236d3e61034a87e50798b0b74
Galen Ferrara <galen.ferrara557@gmail.com>
2025-06-27 13:33:22 +0100

    Fixed bug which caused a crash when the bot didn't make any trades

--- strategies/macd_improved.py
+++ strategies/macd_improved.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 def run_macd_strategy_improved(data):
@@ -37,6 +36,10 @@ and sell when the MACD crosses below the signal line.
 def crossover_logic(data):
     position = 'neutral' # No position by default
 
+    # Position columns must be initialized here, otherwise an error will occur in cases where no trade is made
+    data['Position'] = 0            # Initialize Position column
+    data['Position_Change'] = 0     # Initialize Position_Change column
+
     for i in range(1, len(data)-1):
         histogram_crosses_above_zero = data['Histogram'].iloc[i] > 0 and data['Histogram'].iloc[i-1] <= 0   # If the MACD is above the signal line (Short term upwards trend)
         macd_below_zero = data['MACD'].iloc[i] < 0                                                          # If the MACD is below the centerline (Undervalued)
@@ -53,7 +56,6 @@ def crossover_logic(data):
 
     return data
 
-
 def buy_operation(data, index):
     """Marks a buy signal and sets Position and Position_Change appropriately."""
     data.at[index, 'Position'] = 1
@@ -62,7 +64,7 @@ def buy_operation(data, index):
 def sell_operation(data, index):
     """Marks a sell signal and sets Position and Position_Change appropriately."""
     data.at[index, 'Position'] = 0
-    data.at[index, 'Position_Change'] = 0
+    data.at[index, 'Position_Change'] = -1
 
 def plot_strategy(data):
     """
@@ -94,7 +96,7 @@ def plot_strategy(data):
         ax2.axvline(x=idx, color='green', label='', alpha=0.4)
 
     # Plot sell signals
-    sell_signals = data[data['Position_Change'] == 0]
+    sell_signals = data[data['Position_Change'] == -1]
     for idx in sell_signals.index:
         ax1.axvline(x=idx, color='red', label='', alpha=0.4)
         ax2.axvline(x=idx, color='red', label='', alpha=0.4)
